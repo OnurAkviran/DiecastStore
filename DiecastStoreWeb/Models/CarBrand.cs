28e7ff1218d2e8525335449e61557d8b9eee3e43
Onur Akviran <oakviran@gmail.com>
2025-08-27 20:20:57 +0300

    Add project files.

---
+++ DiecastStoreWeb/Models/CarBrand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiecastStoreWeb.Models
+{
+    public class CarBrand
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CarBrandName { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
