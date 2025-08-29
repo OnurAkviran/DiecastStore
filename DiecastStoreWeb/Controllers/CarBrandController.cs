c0ed9958c0f3d69179630966a29318176600defe
Onur Akviran <oakviran@gmail.com>
2025-08-29 18:40:15 +0300

    Add CarBrand CRUD endpoints and toastr notifications

--- DiecastStoreWeb/Controllers/CarBrandController.cs
+++ DiecastStoreWeb/Controllers/CarBrandController.cs
@@ -31,9 +31,64 @@ namespace DiecastStoreWeb.Controllers
             {
                 _dbContext.CarBrands.Add(brand);
                 _dbContext.SaveChanges();
+                TempData["success"] = "Brand created succesfully.";
                 return RedirectToAction("Index");
             }
             return View(brand);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            CarBrand? carBrand = _dbContext.CarBrands.Find(id);
+            if (carBrand == null)
+            {
+                return NotFound();
+            }
+            return View(carBrand);
+        }
+        [HttpPost]
+        public IActionResult Edit(CarBrand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.CarBrands.Update(brand);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Brand updated succesfully.";
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            CarBrand? carBrand = _dbContext.CarBrands.Find(id);
+            if (carBrand == null)
+            {
+                return NotFound();
+            }
+            return View(carBrand);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            CarBrand? carBrand = _dbContext.CarBrands.Find(id);
+
+            if (carBrand == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Remove(carBrand);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Brand deleted succesfully.";
+            return RedirectToAction("Index");
         }
     }
 }
